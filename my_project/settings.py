a9f2de63f1557530e996d0c976fae5ad8a8c7169
Seema Vij <seemavij@live.ie>
2024-03-03 19:27:11 +0000

    add whitenoise

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-al7jmgc1w^)wa77ps!8^a=@4x(6wxk&qbc7x1_o0&!-xx4zr-('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-seemavij-booking-365iimojdqj.ws-eu108.gitpod.io',
 '.herokuapp.com']
