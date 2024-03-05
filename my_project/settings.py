2c2e5f6cb592ba349aa3f078b418c314d70499ba
Seema Vij <seemavij@live.ie>
2024-03-05 18:14:52 +0000

    add correction

--- my_project/settings.py
+++ my_project/settings.py
@@ -15,6 +15,7 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
+    
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,9 +90,7 @@ DATABASES = {
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
