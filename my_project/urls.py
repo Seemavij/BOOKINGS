a15167b88d76529dbf2eeca29419b5e24c452502
Seema Vij <seemavij@live.ie>
2024-03-08 18:05:59 +0000

    env.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from bookings import views as bookings_posts
 
 urlpatterns = [
+    path("bookings/", blog_views.my_blog, name='bookings'),
     path('admin/', admin.site.urls),  
 ]
