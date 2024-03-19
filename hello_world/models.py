9df7c6031afcb6d6c7eca38e25db4a4aa0a75f09
Seema Vij <seemavij@live.ie>
2024-03-19 10:39:03 +0000

    added comment crud

--- hello_world/models.py
+++ hello_world/models.py
@@ -1,41 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField(blank=True)
+class About(models.Model):
+    title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ["-created_on"]
+    content = models.TextField()
 
     def __str__(self):
-        return f"{self.title} | written by {self.author}"
+        return self.title
 
 
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ["created_on"]
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Comment {self.body} by {self.author}"
+        return f"Collaboration request from {self.name}"
